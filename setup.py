d4559992e790c88799bdf6105928589f0665d2bc
Bernardo Issenguel <beissenguel12@gmail.com>
2025-12-05 11:18:59 +0100

    setup: improvments

--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
 from setuptools import setup, find_packages
-
-setup(
-    name='py_futuur_client',
-    version='1.0.0',
-    packages=find_packages(),
-    install_requires=[
-        'requests',
-    ],
-)
-
-from setuptools import setup, find_packages
 import os
 
 
